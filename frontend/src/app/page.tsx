0da5c569db76abe966cc8a52372866fc97a8db4b
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-14 05:40:27 -0800

    header component and modify styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,8 @@ import Navbar from "../../components/Navbar"
 
 export default function Home() {
   return (
-      <main>
+      <main className="wrapper ovh">
+        {/* <div className="preloader"></div> */}
         <Navbar />
       </main>
   )
