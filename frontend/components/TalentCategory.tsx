e0f46dd68cd5d577bd7a573ebf256c34659f7523
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-15 13:48:52 -0800

    talent category component

---
+++ frontend/components/TalentCategory.tsx
@@ -0,0 +1,192 @@
+function TalentCategory(){
+    return (
+        <section className="pb40-md pb90">
+        <div className="container">
+            <div className="row align-items-center wow fadeInUp" data-wow-delay="300ms">
+                <div className="col-lg-9">
+                    <div className="main-title2">
+                        <h2 className="title">Explore talent by category</h2>
+                        <p className="paragraph">Find inspiration in 1700+ skills.</p>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <div className="text-start text-lg-end mb-4 mb-lg-2">
+                        <a className="ud-btn2" href="page-service-single.html">All Categories<i
+            className="fal fa-arrow-right-long"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div className="row d-none d-lg-flex wow fadeInUp">
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-developer"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Development & IT</h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-web-design-1"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Creative & Design </h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-digital-marketing"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Digital Marketing</h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-translator"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Writing & Translation</h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-microphone"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Music & Audio</h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-video-file"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Video & Animation</h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-ruler"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Engineering & Architecture</h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="iconbox-style1">
+                        <div className="icon"><span className="flaticon-goal"></span></div>
+                        <div className="details mt20">
+                            <p className="text mb5">1.800 skills</p>
+                            <h4 className="title">Finance & Accounting</h4>
+                            <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row d-block d-lg-none">
+                <div className="col-lg-12">
+                    <div className="slider-outer-dib navi_pagi_top_right slider-5-grid owl-carousel owl-theme wow fadeInUp">
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-developer"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Development & IT</h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-web-design-1"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Creative & Design </h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-digital-marketing"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Digital Marketing</h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-translator"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Writing & Translation</h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-microphone"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Music & Audio</h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-video-file"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Video & Animation</h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-ruler"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Engineering & Architecture</h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="iconbox-style1">
+                                <div className="icon"><span className="flaticon-goal"></span></div>
+                                <div className="details mt20">
+                                    <p className="text mb5">1.800 skills</p>
+                                    <h4 className="title">Finance & Accounting</h4>
+                                    <p className="mb-0">Software Engineer, Master of Web and Mobile Development, and More</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default TalentCategory
