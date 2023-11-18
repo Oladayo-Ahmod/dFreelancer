f12459fa35a441344f80e3e67632d02fa7938933
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-18 00:04:44 -0800

    context api

--- frontend/context/MarketPlace.tsx
+++ frontend/context/MarketPlace.tsx
@@ -7,7 +7,30 @@ import Router from 'next/router';
 import Swal from 'sweetalert2';
 import Freelancer from '@/app/interfaces/freelancerProps';
 
-export const GOVERNANCE_CONTEXT = createContext<Freelancer | undefined>(
+export const FREELANCER_CONTEXT = createContext<Freelancer | undefined>(
     undefined
   );
 
+let connect : any
+if(typeof window !=='undefined'){
+    connect = (window as any).ethereum
+}
+
+const FreelancerProvider:React.FC<{children : React.ReactNode}>=({children,})=>{
+
+    // states variables
+    const [account, setAccount] = useState<string>()
+    const [deployer, setDeployer] = useState<string>();
+
+
+    return(
+        <FREELANCER_CONTEXT.Provider
+        value={{
+            connect
+        }}
+        >
+
+        </FREELANCER_CONTEXT.Provider>
+    )
+
+}
