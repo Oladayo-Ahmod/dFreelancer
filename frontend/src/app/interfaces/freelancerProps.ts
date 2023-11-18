20b8b28e91c514ff0671eb84dd8b4c584b11ee57
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-18 00:37:04 -0800

    register freelancer interface

--- frontend/src/app/interfaces/freelancerProps.ts
+++ frontend/src/app/interfaces/freelancerProps.ts
@@ -1,8 +1,21 @@
+import React, { SetStateAction } from "react"
+
 interface FreelancerProps{
 
     account : string | undefined
     connectWallet : ()=>void
-    
+    freelancerForm : {
+        name : string | undefined,
+        country : string | undefined,
+        skills : string  | undefined,
+        imageURL : string | undefined
+    }
+    setFreelancerForm : React.Dispatch<SetStateAction<{
+        name : string | undefined,
+        country : string | undefined,
+        skills : string  | undefined,
+        imageURL : string | undefined
+    }>>
 }
 
 export default FreelancerProps
