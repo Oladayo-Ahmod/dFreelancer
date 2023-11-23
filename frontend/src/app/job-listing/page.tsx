88581fdb7f9b51b598da1d0873e9fcb07b7150dc
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-23 03:34:54 -0800

    jobs listing page

--- frontend/src/app/job-listing/page.tsx
+++ frontend/src/app/job-listing/page.tsx
@@ -1,8 +1,19 @@
+import Footer from "../../../components/Footer"
+import MobileNavbar from "../../../components/MobileNavbar"
+import Navbar from "../../../components/Navbar"
+
 function JobListing(){
-    return(
-        <>
-        
-        </>
+    return (
+        <main className="wrapper ovh">
+              {/* <div className="preloader"></div> */}
+            <Navbar />
+            <MobileNavbar />
+            <div className="body_content">
+               <JobListing />
+                <Footer />
+            </div>
+        </main>
     )
 }
+
 export default JobListing
