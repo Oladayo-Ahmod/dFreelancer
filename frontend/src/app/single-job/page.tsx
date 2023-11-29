dab46f2dfd0ec6fb4a08eec1ea3e44bd6ed03a1d
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-29 05:51:51 -0800

    render single job

--- frontend/src/app/single-job/page.tsx
+++ frontend/src/app/single-job/page.tsx
@@ -1,6 +1,22 @@
+import Footer from "../../../components/Footer"
+import JobListing from "../../../components/JobListing"
+import MobileNavbar from "../../../components/MobileNavbar"
+import MyJobs from "../../../components/MyJobs"
+import Navbar from "../../../components/Navbar"
+
+
 function SingleJob(){
     return (
-
+        <main className="wrapper ovh">
+              {/* <div className="preloader"></div> */}
+            <Navbar />
+            <MobileNavbar />
+            <div className="body_content">
+               <JobListing />
+               <MyJobs />
+                <Footer />
+            </div>
+        </main>
     )
 }
 
