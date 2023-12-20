f12bc949d3c054a32259479ffdffe9bf16d51288
oladayo ahmod <oladayoahmod112@gmail.com>
2023-12-20 07:37:41 -0800

    chat functionality

--- frontend/src/app/chat/page.jsx
+++ frontend/src/app/chat/page.jsx
@@ -1,15 +1,19 @@
 import '@sendbird/uikit-react'
 import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider'
+import CustomizedChat from '../../../components/CustomizedChat'
 
 function Chat(){
-    const APP_ID = process.env.SENDBIRD_APP_ID
+    const APP_ID = process.env.SENDBIRD_APP_ID 
     const USER_ID = process.env.SENDBIRD_USER_ID
     const USERNAME = process.env.SENDBIRD_USERNAME
     const ACCESS_TOKEN = process.env.SENDBIRD_ACCESS_TOKEN
 
     return (
-        <SendbirdProvider >
-
+        <SendbirdProvider 
+        appId={APP_ID}
+        userId={USER_ID}
+        >
+            <CustomizedChat />
         </SendbirdProvider>
     )
 }
