c6209712842a58eaddf5b79275ffcfd1403dc50f
oladayo ahmod <oladayoahmod112@gmail.com>
2023-12-20 04:10:40 -0800

    chat page

---
+++ frontend/src/app/chat/page.tsx
@@ -0,0 +1,17 @@
+import '@sendbird/uikit-react'
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider'
+
+function Chat(){
+    const APP_ID = process.env.SENDBIRD_APP_ID
+    const USER_ID = process.env.SENDBIRD_USER_ID
+    const USERNAME = process.env.SENDBIRD_USERNAME
+    const ACCESS_TOKEN = process.env.SENDBIRD_ACCESS_TOKEN
+
+    return (
+        <SendbirdProvider>
+
+        </SendbirdProvider>
+    )
+}
+
+export default Chat
